f8ccd8f462746f1b6ac73a25cb026be729261389
Luke1222 <darumb@gmail.com>
2018-10-16 15:47:58 +0300

    Penktas PHP data

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
+// Nustatome Zoną į Vilno
 date_default_timezone_set('Europe/Vilnius'); ?>
 <html>
-    <title><?php echo 'Aš,' . ' ' . date('l') . ' ' . "ir PHP"?></title>  
-    <h2> <?php echo 'Darius - HTML' . ' '. "ir PHP asas jau nuo" . ' ' . date('Y') . ' ' . 'metų'?></h2>
-    <p><?php echo  'Viskas prasidėjo'. ' ' . date("m") . ' ' . 'mėnesio' . ' ' . date('d') . ' ' . 'dieną'; ?></p>
+    <!-- Pagrindinė puslapio antraštė -->
+    <title><?php echo 'Php lydės ir' . ' ' . date(("Y/m/d"), strtotime('+' . rand(0,10) . 'year'));?></title> 
+    <h2  style="background: rgb(<?php print rand(0, 255).",". rand(0,  255).",". rand(0, 255)?>)"> <?php echo 'Darius -' . ' '. "Galbūt turėsiu " . ' ' . rand(1,5) . ' ' . 'vaikų';?></h2>
+    <!-- Body 2 -->
+    <p style="font-size: <?php print rand(0, 255)?>"><?php echo  'D Trumpas nebebus prezidentu' . ' ' . date("Y/m/d", strtotime('+' . rand(0, 10) . 'year'));?></p>
 </html>
-
+<?php
+// Galiu komentuoti kiek noriu
+print "php ir aš"; // Galiu ir ne
+/* ilgas komentaras
+ * reikalas
+ * rimtas
+ */
+?>
